657cdabee21c570918f510eed719a071f82857a6
qweku2000 <joeadofonyarko@gmail.com>
2023-10-16 05:35:28 -0700

    Task 1

---
+++ 0x0C-python-almost_a_circle/models/README.md
@@ -0,0 +1,1 @@
+A rview of python oop
---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Define the base class """
+
+class Base:
+    """ This class will be the “base” of all other classes in this project. The goal of it is to manage id attribute in all your future classes and to avoid duplicating the same code (by extension, same bugs) """
+    
+    __nb_objects = 0
+    def __init__(self, id=None):
+        if self.id is not None:
+            self.id = id
+
+        else:
+            Base.__nb_objects = Base.__nb_objects + 1
+            self.id = Base.__nb_objects
