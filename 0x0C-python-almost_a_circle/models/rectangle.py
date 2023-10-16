677d5d697ffa144da57d265ffffdfee3bd379b69
qweku2000 <joeadofonyarko@gmail.com>
2023-10-16 08:33:31 -0700

    cannot set attributes now

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
 
+from models.base import Base
+
 class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         self.width = width
         self.height = height
         self.x = x
         self.y = y
+        super().__init__(id)
 
+    @property
+    def width(self):
+        return self.__width
+    
     @width.setter
     def width(self,value):
         if type(value) is not int:
@@ -14,10 +21,10 @@ class Rectangle(Base):
         if value <= 0:
             raise ValueError("width must be > 0")
         self.__width = value
-    
-    @width.getter
-    def width(self):
-        return self.__width
+  
+    @property
+    def height(self):
+        return self.__height
 
     @height.setter
     def heigt(self,value):
@@ -26,9 +33,11 @@ class Rectangle(Base):
         if value <= 0:
             raise ValueError("Height must be > 0 ")
         self.__height = value
+    
 
-    def height(self):
-        return self.__height
+    @property
+    def x(self):
+        return self.__x
 
     @x.setter
     def x(self,value):
@@ -36,12 +45,12 @@ class Rectangle(Base):
             raise TypeError("x must be an integer")
         if x <= 0 :
             raise TypeError("x must be >= 0")
-        self__x = value
+        self.__x = value
 
-    @x.setter
-    def x(self):
-        return self.__x
-
+    @property
+    def y(self):
+        return self.__y
+        
     @y.setter
     def y(self,value):
         if type(value) is not int:
