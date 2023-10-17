a250a2a569d9523fb9a4777a42c95d0f20c6cd93
qweku2000 <joeadofonyarko@gmail.com>
2023-10-17 05:53:34 -0700

    Adding comments to the code

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-
+"""Defines a Rectangle class"""
 from models.base import Base
 
-"""Define a rectangle class"""
 class Rectangle(Base):
     """Represent a rectangle"""
     def __init__(self, width, height, x=0, y=0, id=None):
